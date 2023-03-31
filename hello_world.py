20d7906e479a78872260f1736aa49451f563100d
akmaghakian <129209752+akmaghakian@users.noreply.github.com>
2023-03-31 01:16:04 +0100

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+#Firstly the terminal requests the user to input a name
+name = input ("Enter your name, please:")
+#Secondly the terminal prints the name
+print (name)
+#Thirtdly the terminal requests the user to input an age
+age =input ("Please enter your age:")
+#Fourthtly the terminal prints the age
+print (age)
+#Fifthly the terminal is instructed to print "Hello World"
+print ("Hello World!")
---
+++ menu.py
@@ -0,0 +1,12 @@
+#The Terminal will first ask the user to insert the favourite foods one by one
+
+item1 = input("Enter your first favorite food: ")
+item2 = input("Enter your second favorite food: ")
+item3 = input("Enter your third favorite food: ")
+
+#The terminal will return a confirmation string plus the list of items ordered
+
+print ("Order Confirmation! You have ordered: ")
+print(item1)
+print(item2)
+print(item3)
